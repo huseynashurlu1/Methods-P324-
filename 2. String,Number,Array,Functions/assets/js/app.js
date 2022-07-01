8f11c0ad00ca0c733fc3e277cbfc112849d7b3bd
huseynashurlu1 <98535007+huseynashurlu1@users.noreply.github.com>
2022-07-01 21:39:24 +0400

    Add files via upload

---
+++ 2. String,Number,Array,Functions/assets/js/app.js
@@ -0,0 +1,182 @@
+
+// var ad = '   Seymur Manafov code academyde oxuyur.Seymur manafov axsam qrupundadir     ';
+// var firstname = new String('Seymur');
+
+// var new_ad = ad.trimStart();
+
+// console.log(new_ad.length);
+// console.log(ad.slice(-2));
+// console.log(ad.replace(/Manafov/gi,'Manafli'));
+// console.log(ad.toLowerCase());
+// console.log(ad.toUpperCase());
+
+
+// var x = 'Code Academy asaasasasas. Code Academy';
+// console.log(x.charCodeAt(0));
+// console.log(x.indexOf('Code',10));
+
+// var ad = 'Seymur';
+// var yas = 25;
+
+// // Template Literals
+// console.log('Telebenin adi ' + ad + "dur.Onun " + yas + " yasi var");
+// console.log(`Telebenin adi ${ad}dir.Onun ${yas} yasi var`);
+
+// var x = "20";
+// var y = new Number(20);
+
+// // x is Not a Number
+// console.log(isNaN(x));
+
+// // toString // toFixed // toExponential // toPrecision // valueOf
+
+
+// var a = 20;
+// var b = a.toString();
+// console.log(typeof b);
+
+
+
+// var num = 5.67;
+// var pi = Math.PI;
+// // console.log(pi.toFixed(2));
+// console.log(num.toFixed(1));
+
+
+// var x = +'20';
+// var new_x = Number(x);
+
+
+
+var colors = ['red','white','black','grey'];
+// var color = new Array('red','white','black','grey')
+// var users = [];
+
+
+// pop / push / shift / unshift
+// colors.push('yellow');
+// colors.pop()
+// colors.unshift('green')
+// colors.shift()
+// console.log(colors);
+
+// colors = [];
+// colors.length = 0
+// delete colors[0]
+
+// console.log(colors.toString());
+
+// var front_end = ['html','css','js'];
+// var back_end = ['c#','sql','server'];
+// var full_stack = front_end.concat(back_end)
+// full_stack.sort();
+// full_stack.reverse()
+// console.log(full_stack);
+// console.log(full_stack.splice(0,3));
+var full_stack = ['html','css','js','c#','sql','server'];
+
+// for(var i=0;i<full_stack.length;i+=2) {
+//     document.write(full_stack[i] + "<br>")
+// }
+
+// for(var x in full_stack) {
+//     console.log(full_stack[x]);
+// }
+
+// for(var i of full_stack) {
+//     console.log(i);
+// }
+
+// for(var i=0;i<100;i++) {
+//     if(i==10) {
+//         console.log('Bura 10dur');
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+// var x = 10;
+// console.log(++x);
+
+// var i = 0;
+
+// // while (i<100) {
+// //     i++;
+// //     console.log(i);
+// // }
+
+// do {
+//     i++;
+//     console.log(i);
+// } while (i<50);
+
+
+// function P324() {
+//     console.log('Hello P324');
+// }
+
+// P324();
+
+
+// function Hello(firstname) {
+//     // console.log('Hello ' + firstname);
+//     return firstname;
+// }
+
+// console.log(Hello('Cingiz'));
+
+
+// var text = document.getElementById('text');
+// // console.log(text);
+
+// function P324() {
+//     // // text.innerHTML = 'FULL-STACK DEVELOPMENT'
+//     // text.style.color = 'red'
+//     // text.style.letterSpacing = '5px'
+// }
+
+
+// var img = document.getElementById('photo');
+// function P324() {
+//     if(img.style.filter === 'grayscale(1)') {
+//         img.style.filter = 'grayscale(0)' 
+//         img.style.transform = 'rotate(90deg)'
+
+//     }
+//     else{
+//         img.style.filter = 'grayscale(1)'
+//         img.style.transform = 'rotate(0deg)'
+//     }
+// }
+
+
+
+
+// var body = document.body;
+// var icon = document.getElementById('mode')
+// function P324() {
+//     if(body.style.backgroundColor === 'black') {
+//         body.style.backgroundColor = 'white';
+//         body.style.color = 'black';
+//         icon.className = 'fa-solid fa-moon'
+//     }
+//     else{
+//         body.style.backgroundColor = 'black';
+//         body.style.color = 'white';
+//         icon.className = 'fa-solid fa-sun'
+//     }
+// }
+
+
+// function Change() {
+//     // console.log(value);
+//     var value = document.getElementById('input').value;
+//     var div = document.getElementById('first');
+//     div.style.borderRadius = `${value}`
+// }
+
+
+// 1 ədəd input olacaq və istifadəçi inputa Code Academy-dəki Qrup adını yazacaq.
+//Əgər səhər qrupudursa HTML-ə əlavə olunacaq qrup adının rəngi sarı olsun, 
+//günorta qrupu qırmızı olsun, axşam qrupu isə qara rəngdə olsun
+
